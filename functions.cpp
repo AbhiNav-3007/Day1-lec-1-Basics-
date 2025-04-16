6fa22a834003ea4ae2584f37ba88b7930f519265
AbhiNav-3007 <1DT23CY002@dsatm.edu.in>
2025-04-16 22:10:24 +0530

    basics functions

---
+++ functions.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+void printname(string name){
+    cout<<"hey"<<name<<endl;
+}
+int main(){
+    string name;
+    cin>>name;
+    printname(name);
+
+    string name2;
+    cin>>name2;
+    printname(name2);
+
+}
