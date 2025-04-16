fc1b77d99001207dd27998bd61b04ae0a2cedc55
AbhiNav-3007 <1DT23CY002@dsatm.edu.in>
2025-04-17 01:31:28 +0530

    functions:- pass by reference

--- functions3.cpp
+++ functions3.cpp
@@ -2,16 +2,29 @@
 // pass by value
 #include<bits/stdc++.h>
 using namespace std;
-void dosomething(int num){
-    cout<<num<<endl;
-    num+=5;
-    cout<<num<<endl;
-    num+=5;
-    cout<<num<<endl;
+// void dosomething(int num){
+//     cout<<num<<endl;
+//     num+=5;
+//     cout<<num<<endl;
+//     num+=5;
+//     cout<<num<<endl;
+// }
+// int main(){
+//     int num=10;
+//     dosomething(num);
+//     cout<<num<<endl;
+// }
+// in pass by reference value in main is being copy to function ,original value remains same
+
+
+// pass by reference
+void dosomething(string &s){
+    s[0]='t';
+    cout<<s<<endl;
 }
 int main(){
-    int num=10;
-    dosomething(num);
-    cout<<num<<endl;
+    string s="raj";
+    dosomething(s);
+    cout<<s<<endl;
+    return 0;
 }
-// in pass by reference value in main is being copy to function ,original value remains same
