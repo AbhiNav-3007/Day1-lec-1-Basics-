2968843171b790ba199a75b5479924e3b79f030f
AbhiNav-3007 <1DT23CY002@dsatm.edu.in>
2025-04-16 22:18:14 +0530

    basics functions2

---
+++ functions2.cpp
@@ -0,0 +1,10 @@
+// take two numbers and prints its sum
+#include<bits/stdc++.h>
+using namespace std;
+int printsum(int num1,int num2){
+    int sum = num1 + num2;
+    return sum;
+}
+int main(){
+    cout<<printsum(2,3);
+}
